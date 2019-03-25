1084683e33e68c708ae741081fb4f5d6cd670e24
deepakbeatz <42633041+deepakbeatz@users.noreply.github.com>
2019-03-26 00:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from bottle import get,post,route,run,template,TEMPLATE_PATH,static_file,request
 def index():
     return "deployed on Heroku!"
 
-run(host="0.0.0.0",port=int(os.environ.get('PORT',5000))
+run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
